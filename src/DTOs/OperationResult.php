17d1d6a37cdb7b1d2e0855e727bcc7138aed614e
Christian Bustamante <tihloh12@gmail.com>
2026-08-29 15:37:15 +0800

    Trace user operation results

--- src/DTOs/OperationResult.php
+++ src/DTOs/OperationResult.php
@@ -2,10 +2,21 @@
 
 namespace Tihloh\Prefab\Users\DTOs;
 
+use Tihloh\Prefab\PrefabRuntime;
+
 final class OperationResult
 {
     public function __construct(
         public mixed $data,
         public array $log,
-    ) {}
+    ) {
+        $operation = (string) ($log['action'] ?? 'operation');
+        PrefabRuntime::traceStart('users', $operation, [
+            'actor_id' => $log['actor_id'] ?? null,
+            'subject_id' => $log['subject_id'] ?? null,
+        ]);
+        PrefabRuntime::traceEnd([
+            'result' => is_object($data) ? $data::class : get_debug_type($data),
+        ]);
+    }
 }
